0e13f3df2d6a8afde05ee06a9991bb6eba5a96fd
kalaha <kalaha.pro@gmail.com>
2022-07-30 00:22:09 +0400

    Importation du api et vérification si tout est importé, done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchJson();
+  }, []);
+
+
+  //importation de l'api jsonplaceholder
+  const fetchJson = async () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+       //Gestion d'erreur si la promise n'est pas réalisée
+      .catch((error) => console.log(error));
+  };
+
+/*
+  vérification de l'importation si on la liste des utilisateurs ou pas. 
+  PS: dans la console bien sûr.
+  Résultat: on a bien les utilisateurs.
+*/
+  console.log(users);
+
   return (
     <div className="App">
+      <h1>Taiwa Project Final Interview</h1>
     </div>
   );
-}
+};
 
 export default App;
