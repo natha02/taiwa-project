3629f355d422e7db118b0bf4f64d68b491ff65e4
kalaha <kalaha.pro@gmail.com>
2022-07-31 01:28:32 +0400

    création de la navigation et ajout des liens et la rediréctions avec style, Done.

--- src/components/navigations/Navigation.js
+++ src/components/navigations/Navigation.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import { faAdd, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import "./navigation.css";
 
+/*
+création de nav bar avec les différents liens
+- utilisation du librairie FontAwesomeIcon pour les icones
+*/
 const Navigation = () => {
   return (
     <>
         <nav>
-            <ul>
-                <li><a href='#header'>Home</a></li>
-                <li><a href='#add-user'>Add User</a></li>
-                <li><a href='#list-user'>List User</a></li>
-            </ul>
+          <div className="nav-content">
+                <div><a href='#header'><FontAwesomeIcon icon={faHome} color="#082032" className='icon'/></a></div>
+                <div><a href='#add-user'><FontAwesomeIcon icon={faAdd} color="#082032" className='icon'/></a></div>
+                <div><a href='#list-user'><FontAwesomeIcon icon={faUser} color="#082032" className='icon'/></a></div>
+          </div>
         </nav>
     </>
   )
