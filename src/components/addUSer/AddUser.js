5b8e1bb4c2af7cacbce823aab73d05e8bf8d2c9a
kalaha <kalaha.pro@gmail.com>
2022-08-01 18:38:53 +0400

    ajout assisstance vocale pour remplir vocalement le formulaire, mais il y a encore des bugs.Done

--- src/components/addUSer/AddUser.js
+++ src/components/addUSer/AddUser.js
@@ -1,8 +1,42 @@
 import React from "react";
 import "./add.css";
-
+import { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web"; //c'est pour le boutton de alan (micro en bas a droite)
+import Typewriter from "typewriter-effect";
 
 const AddUser = ({ fetchAdd }) => {
+  /*
+    intégration voice alan ai pour remplir le formulaire avec le vocal
+
+    importation de la clé pour l'utilisation de alan ai et pour identification de mes réglages aussi. (connexion)
+  */
+  const Alan_key = `083ae6733331e0dc685b73ec325a5d342e956eca572e1d8b807a3e2338fdd0dc/stage`;
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    alanBtn({
+      key: "083ae6733331e0dc685b73ec325a5d342e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        //test dans la console si alan reprends vraiment ce qu'on dit
+        console.log(commandData);
+
+        //condition de remplissage du form
+        if (commandData.command === "username") {
+          setUsername(commandData.data);
+        }
+
+        if (commandData.command === "name") {
+          setName(commandData.data);
+        }
+
+        if (commandData.command === "email") {
+          setEmail(commandData.data);
+        }
+      },
+    });
+  }, []);
+
   /*
     onSubmit vas ajouter les valuer qu'on séléctionne dans fetchAdd qu'on a préalablement construis
     et vas reinitilialiser le formulaire.
@@ -16,13 +50,11 @@ const AddUser = ({ fetchAdd }) => {
     const username = e.target.username.value;
     const email = e.target.email.value;
     fetchAdd(name, username, email);
-  
+
     e.target.username.value = "";
     e.target.name.value = "";
     e.target.email.value = "";
   };
-
-
 
   /*
     on a juste crée une formulaire avec username , name , email 
@@ -31,13 +63,57 @@ const AddUser = ({ fetchAdd }) => {
   return (
     <div className="add-user" id="add-user">
       <div className="content">
-      <h3>Add User</h3>
-      <form onSubmit={onSubmitForm}>
-        <input type="text" placeholder="Username" name="username" />
-        <input type="text" placeholder="Name" name="name" />
-        <input type="email" placeholder="Email" name="email" />
-        <button onSubmit={onSubmitForm}>ADD</button>
-      </form>
+        <h3>Add User</h3>
+        <p>
+          Notre ami "Alan ai" peut nous aider à remplir le formulaire à l'aide
+          du vocal. Il suffit de cliquer sur boutton "micro en bas à droite".
+          <br />
+          Il suffit de lui dire les mots magiques suivante:
+          <br />
+        </p>
+
+        <h5>
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              delay: 40,
+              strings: [
+                "My name is ... 🎤🎙️",
+                "My Username is ... 🎤🎙️",
+                "My Email is ... 🎤🎙️"
+              ],
+            }}
+          />
+        </h5>
+
+        <form onSubmit={onSubmitForm}>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+
+          <button onSubmit={onSubmitForm}>ADD</button>
+        </form>
       </div>
     </div>
   );
